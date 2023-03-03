3bc16afece3b5a0f10e6018c3bb112a565562407
werasak mayer <jokerwwe0542@gmail.com>
2023-03-03 15:17:13 +0700

    "lab23"

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+int main(int argc,char *argv[]){
+
+    if(argc == 1){
+        cout <<"Please input numbers to find average.";
+    }else{
+    cout << "---------------------------------"<<endl;
+    float sum=0;
+    for(int i= 1; i<argc;i++){
+        sum+=atof(argv[i]);
+    }
+    cout << "Average of "<< argc-1 <<" numbers = "<< sum/(argc-1)<<endl;
+    cout << "---------------------------------";
+    }
+    return 0;
+}
